08c5a504b7d31cf1f579a4f5b991d81e9302c13c
avi <avkan1087@gmail.com>
2020-04-16 18:54:44 -0400

    mvp done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,32 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
 
+def rps_recursion(number, list):
+    if number == 0:
+        return number
+
+
+def rock_paper_scissors(n):
+    result_array = []
+    selection = ['rock', 'paper', 'scissors']
+
+    def recursive_listing(iterations, result=[]):
+        if iterations == 0:
+            return result_array.append(result)
+        for choice in selection:
+            recursive_listing(iterations - 1, result + [choice])
+
+    recursive_listing(n)
+    return result_array
+    return return_list
+
+
+rock_paper_scissors(2)
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
